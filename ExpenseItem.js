f35ba9553cdc55c53999c1f652b9d77f2daa56cd
Taufeeque0020 <116538249+Taufeeque0020@users.noreply.github.com>
2022-11-28 14:33:03 +0530

    Add files via upload

---
+++ ExpenseItem.js
@@ -0,0 +1,18 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>November 28th 2022</div>
+      <div>
+        <h2>Expense Items</h2>
+        <h3>Food</h3>
+        <div>Rs 10</div>
+        <h3>Petrol</h3>
+        <div>Rs 100</div>
+        <h3>Movies</h3>
+        <div>Rs 200</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
